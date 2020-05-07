b1c11328a1a0e40612e9d2ccec60064de2ffed4b
awu556 <austinw556@gmail.com>
2020-05-07 10:50:55 -0400

    more test cases for insert

--- BinarySearchTree.test.js
+++ BinarySearchTree.test.js
@@ -20,9 +20,19 @@ describe("Binary Search Tree class", () => {
       expect(testBST.insert).not.toBeUndefined();
     });
 
-    test("should insert values into the BST", () => {
+    test("first value inserted should be the root value", () => {
       let test = bulkInsert([10, 20, 30, 40]);
       expect(test.root.data).toEqual(10);
+    });
+
+    test("every value added less than the root should be on the left branch", () => {
+      let test = bulkInsert([20, 16, 31, 8]);
+      expect(test.root.left.data).toBe(16);
+    });
+
+    test("every value added greater than the root should be on the right branch", () => {
+      let test = bulkInsert([20, 16, 31, 8]);
+      expect(test.root.right.data).toBe(31);
     });
   });
 });
